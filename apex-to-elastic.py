8e6bd2f273aed8ffce64e887f5cf1808a34e8f89
JeffTech <jeff@unbiasedgeek.com>
2022-03-05 21:31:57 -0700

    Added catch up mechanism

--- apex-to-elastic.py
+++ apex-to-elastic.py
@@ -13,23 +13,45 @@ base_url      = os.environ.get("apex-url")
 elastic_url   = os.environ.get("elastic-url")
 elastic_index = os.environ.get("elastic-index")
 
-now = datetime.now() - timedelta(minutes=1)
+es = Elasticsearch(elastic_url)
+
+result = es.search(
+    index=elastic_index,
+    body={
+        "sort": [
+            {
+            "timestamp": {
+                    "order": "desc"
+                }
+            }
+        ],
+        "size": 1
+    }
+)
+
+now = datetime.strptime(result["hits"]["hits"][0]["_source"]["timestamp"].replace(':', ''), "%Y-%m-%dT%H%M%S%z" ) + timedelta(minutes=1)
 
 url = base_url + '/cgi-bin/datalog.xml?sdate=' + now.strftime("%y%m%d%H%M")
 response = requests.get(url)
 data = xmltodict.parse(response.content)
-data["datalog"]["record"][0]["timestamp"] = now.astimezone().isoformat()
 
-es_doc = {}
-es_doc["probes"] = {}
 
-es_doc['timestamp'] = now.astimezone().isoformat()
+print(now.astimezone().isoformat())
 
-for probe in data["datalog"]["record"][0]["probe"]:
-    es_doc['probes'][probe['name']] = float(probe['value'])
+records = data["datalog"]["record"]
+print ( "Processing " + str(len(records)) )
 
-print(json.dumps(es_doc))
+for record in records: 
+    es_doc = {}
+    es_doc["probes"] = {}
 
-es = Elasticsearch(elastic_url)
-resp = es.index(index=elastic_index, document=es_doc)
-print(resp['result'])
+
+    es_doc['timestamp'] = datetime.strptime(record["date"],"%m/%d/%Y %H:%M:%S").astimezone().isoformat();
+
+    for probe in record["probe"]:
+        es_doc['probes'][probe['name']] = float(probe['value'])
+
+    print(json.dumps(es_doc))
+
+    resp = es.index(index=elastic_index, document=es_doc)
+    print(resp['result'])
